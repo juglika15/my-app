b21623ebdd6d64e95f6df9f22e35249cc632a676
juglika <lashajugeli96@gmail.com>
2024-09-30 18:43:03 +0400

    feat: display assignment3 table

--- src/Pages/Assignment3/Assignment3.jsx
+++ src/Pages/Assignment3/Assignment3.jsx
@@ -1,17 +1,39 @@
+import "./Assignment3.css";
+
 const obj = {
   id: "10002",
-
   name: "Eco-Friendly Water Bottle",
-
   description: "Stay hydrated with our durable, eco-friendly water bottle.",
-
   price: 14.99,
-
   currency: "USD",
-
   imageURL: "https://example.com/images/product-10002.jpg",
 };
 
 export default function Assignment3() {
-  return <div className="content">{obj.name}</div>;
+  const tableBody = Object.keys(obj).reduce((acc, key, index) => {
+    acc.push(
+      <tr key={key}>
+        <td>{key}</td>
+        <td>{obj[key]}</td>
+        <td>{index}</td>
+      </tr>
+    );
+
+    return acc;
+  }, []);
+
+  const list = (
+    <table>
+      <thead>
+        <tr>
+          <th>key</th>
+          <th>value</th>
+          <th>index</th>
+        </tr>
+      </thead>
+      <tbody>{tableBody}</tbody>
+    </table>
+  );
+
+  return <div className="content">{list}</div>;
 }
