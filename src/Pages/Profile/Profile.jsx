805edc123439238a78b18dc8a81e0a1abe62e2b5
juglika <lashajugeli96@gmail.com>
2024-10-03 03:41:39 +0400

    feat: add profile form and styles

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,5 +1,28 @@
 import "./Profile.css";
 
 export default function Profile() {
-  return <div className="main">profile</div>;
+  return (
+    <div className="main">
+      <form className="form">
+        <div className="name">
+          <label>
+            <span>First Name:</span>
+            <input type="text" name="first name" value="John" />
+          </label>
+          <label>
+            <span>Last Name:</span>
+            <input type="text" name="last name" value="Doe" />
+          </label>
+        </div>
+        <label>
+          <span>Image URL:</span>
+          <input type="url" name="image" />
+        </label>
+        <label>
+          Email:
+          <input type="email" name="email" value="johndoe@gmail.com" />
+        </label>
+      </form>
+    </div>
+  );
 }
