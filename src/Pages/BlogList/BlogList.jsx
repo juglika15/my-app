262fb2c43846d31637889a35009a17f817acff7a
David Tarkhnishvili <datotarkh@gmail.com>
2024-10-06 23:15:00 +0400

    added loading text to blogs page

--- src/Pages/BlogList/BlogList.jsx
+++ src/Pages/BlogList/BlogList.jsx
@@ -6,10 +6,11 @@ import usePosts from "../../assets/hooks/usePosts";
 
 export default function BlogList() {
 
-  const { posts } = usePosts(); 
+  const { posts, loading } = usePosts(); 
 
   return (
     <main className="main">
+      {loading && <p>Loading...</p>}
       {posts.map((post) => (
         <Blog blog={post} key={post.id} />
       ))}
--- src/assets/hooks/usePosts.jsx
+++ src/assets/hooks/usePosts.jsx
@@ -3,15 +3,17 @@ import { useState } from "react";
 
 const usePosts = () => {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     fetch('https://dummyjson.com/posts')
       .then(response => response.json())
-      .then(data => setPosts(data.posts))
+      .then(data => {setPosts(data.posts); setLoading(false)})
       .catch(error => console.error('Error fetching posts:', error));
   }, []);
 
-  return { posts };
+  return { posts, loading };
 };
 
 export default usePosts;
