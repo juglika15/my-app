c8c7e7e74b80cd9aaed5c28dd0ef6ce80a2119ca
David Tarkhnishvili <datotarkh@gmail.com>
2024-10-06 16:41:19 +0400

    added tags and reactions in dinamically in blog component

--- src/Pages/BlogList/Blog/Blog.jsx
+++ src/Pages/BlogList/Blog/Blog.jsx
@@ -3,13 +3,22 @@ import "./Blog.css";
 export default function Blog({ blog }) {
   return (
     <div className="blog-item">
-      <img src={blog.img} alt={blog.title} />
       <div className="blog-content">
         <h3 className="blog-title">{blog.title}</h3>
         <p className="blog-preview">{blog.body}</p>
-        <p className="blog-author">
-          By <em>{blog.author}</em>
-        </p>
+
+        <strong>Tags:</strong>
+        <ul>
+          {blog.tags.map((tag) => (
+            <li>
+              <span className="blog-tags">{tag}</span>
+            </li>
+          ))}
+        </ul>
+        <div style={{ marginTop: "10px" }}>
+          <button>👍 Like {blog.reactions.likes}</button>
+          <button>👎 Dislike {blog.reactions.dislikes}</button>
+        </div>
       </div>
     </div>
   );
