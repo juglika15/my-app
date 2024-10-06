4cc73434388577eb15853b7cc1b2267fc3625e6e
David Tarkhnishvili <datotarkh@gmail.com>
2024-10-06 11:17:02 +0400

    refactor blog component to fit api data

--- src/Pages/BlogList/Blog/Blog.jsx
+++ src/Pages/BlogList/Blog/Blog.jsx
@@ -3,10 +3,10 @@ import "./Blog.css";
 export default function Blog({ blog }) {
   return (
     <div className="blog-item">
-      <img src={blog.imageUrl} alt={blog.title} />
+      <img src={blog.img} alt={blog.title} />
       <div className="blog-content">
         <h3 className="blog-title">{blog.title}</h3>
-        <p className="blog-preview">{blog.preview}</p>
+        <p className="blog-preview">{blog.body}</p>
         <p className="blog-author">
           By <em>{blog.author}</em>
         </p>
