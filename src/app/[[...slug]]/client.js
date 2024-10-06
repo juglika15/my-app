1c18fee040d0ca260569b2bcbbc4eec508e4b4f6
juglika <lashajugeli96@gmail.com>
2024-10-06 16:37:54 +0400

    #1 refactor: rename and move folders for nextjs

--- src/app/[[...slug]]/client.js
+++ src/app/[[...slug]]/client.js
@@ -2,7 +2,7 @@
 
 import dynamic from "next/dynamic";
 
-const App = dynamic(() => import("../../App"), { ssr: false });
+const App = dynamic(() => import("../../Content/Content.jsx"), { ssr: true });
 
 export function ClientOnly() {
   return <App />;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 import "../index.css";
 
 export const metadata = {
@@ -8,8 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="container">
+        <Header />
         <div id="root">{children}</div>
+        <Footer />
       </body>
     </html>
   );
