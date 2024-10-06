0839878d87c9f3a43ef3f1a921e69b78bd0eb506
juglika <lashajugeli96@gmail.com>
2024-10-06 15:51:43 +0400

    #1 refactor: update appjsx, header and foooter

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,15 @@
 import "./App.css";
-// import Header from "./Header/Header";
-// import Content from "./Content/Content";
-// import Footer from "./Footer/Footer";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Contact from "./Pages/Contact/Contact";
-// import About from "./Pages/About/About";
-// import Assignment3 from "./Pages/Assignment3/Assignment3";
-// import PageNotFound from "./Pages/PageNotFound/PageNotFound";
-// import BlogList from "./Pages/BlogList/BlogList";
-// import Profile from "./Pages/Profile/Profile";
+import Header from "./Header/Header";
+import Content from "./Content/Content";
+import Footer from "./Footer/Footer";
 
 function App() {
   console.log(1);
   return (
     <div className="container">
-      <h1>Title</h1>
-      {/* <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Content />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="about" element={<About />} />
-          <Route path="blog" element={<BlogList />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="assignment-3" element={<Assignment3 />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-        <Footer />
-      </Router> */}
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
