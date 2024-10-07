7000329414afb614fb6f40aae6a601dcc71ef863
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-07 10:37:45 +0400

    add useEffect in Products component

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,4 +1,20 @@
+"use client"
+import { useEffect } from "react"
+
+const productsURL = 'https://dummyjson.com/products';
+
 export default function Products() {
+
+  useEffect(()=>{
+    async function fetchProducts() {
+      const response = await fetch('productsURL');
+      const data = await response.json();
+
+      console.log(data);
+      
+    }
+  },[])
+  
   return (
     <main className="main">products</main>
   )
