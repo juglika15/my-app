0ed613e626fd6802d1c36ad596f5901fa5132023
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-07 12:30:16 +0400

    add useState with try catch

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,19 +1,22 @@
 "use client"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { ProductList } from "./ProductList";
 
 const productsURL = 'https://dummyjson.com/products';
 
 export default function Products() {
-
-  useEffect(()=>{
-    async function fetchProducts() {
-      const response = await fetch(productsURL);
-      const data = await response.json();
-
-      console.log(data);
-      
-    }
-
+   const [productList, setProductList] = useState([]);
+  //  console.log(productList);
+   useEffect(()=>{
+     async function fetchProducts() {
+       try {
+         const response = await fetch(productsURL);
+         const data = await response.json();
+         setProductList(data.products);
+        } catch (error) {
+          setProductList([]);
+        }
+      }
     fetchProducts();
   },[])
   
