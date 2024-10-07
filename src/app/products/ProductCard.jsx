8d7557c279b77667d99c06ac387e75ddac65aeda
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-07 15:33:49 +0400

    enter all product details in ProductCard

--- src/app/products/ProductCard.jsx
+++ src/app/products/ProductCard.jsx
@@ -1,7 +1,28 @@
+import "./ProductCard.css"
 export const ProductCard = (props) => {
   return (
-    <div>
-      {props.product.title}
+    <div className="item">
+        <div style={{
+          height: 120
+        }}>
+        <img 
+          src={props.product.images[0]} 
+          alt={props.product.title}
+          className="item-img"
+          style={{
+            height: '100%',
+            width: '100%',
+          }}
+          />
+
+        </div>
+        <h4 className="item-name">{props.product.title}</h4>
+        <div>${props.product.price}</div>
+        <p className="item-desc">{props.product.description}</p>
+        <div>
+          <button className="button">Add to Cart</button>
+          <a href="/" className="moreCardBtn">more details</a>
+        </div>
     </div>
   )
 }
