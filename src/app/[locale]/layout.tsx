d9b9e797da212fa18ba12fd3b36cc4556f07eac3
juglika <lashajugeli96@gmail.com>
2024-11-15 17:07:44 +0400

    refactor: convert [locale] directory files to typescript

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,7 +5,13 @@ import { routing } from '../../i18n/routing';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
-export default async function LocaleLayout({ children, params: { locale } }) {
+export default async function LocaleLayout({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode;
+  params: { locale: 'en' | 'ka' };
+}) {
   // Ensure that the incoming `locale` is valid
   if (!routing.locales.includes(locale)) {
     notFound();
